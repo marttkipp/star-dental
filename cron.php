6ad898cded08cd6ef4fbf64a4638943a04bc2d46
martin <martin@omnis.co.ke>
2019-10-29 18:51:37 +0300

    changes

---
+++ cron.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$url = 'http://simplex-financials.com/star-dental/reporting/daily_report';
+//Encode the array into JSON.
+$patient_details = array();
+//The JSON data.
+$data_string = json_encode($patient_details);
+
+try{                                                                                                         
+
+	$ch = curl_init($url);                                                                      
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+		'Content-Type: application/json',                                                                                
+		'Content-Length: ' . strlen($data_string))                                                                       
+	);                                                                                                                     
+	$result = curl_exec($ch);
+	curl_close($ch);
+}
+catch(Exception $e)
+{
+	$response = "something went wrong";
+	echo json_encode($response.' '.$e);
+}
+
+?>
