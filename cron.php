905aab5703326b8ca3ec6989ac544418ee14e997
martin <martin@omnis.co.ke>
2019-10-30 07:52:08 +0300

    changes

--- cron.php
+++ cron.php
@@ -10,16 +10,15 @@ $data_string = json_encode($patient_details);
 
 try{                                                                                                         
 
-	$ch = curl_init($url);                                                                      
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-		'Content-Type: application/json',                                                                                
-		'Content-Length: ' . strlen($data_string))                                                                       
-	);                                                                                                                   
-
- 
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json',
+		'Content-Length: ' . strlen($data_string))
+	);
 	$result = curl_exec($ch);
 
 	var_dump($result);die();
