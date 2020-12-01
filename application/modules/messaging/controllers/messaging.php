ea494cb08d8438ad19ba2e27f11bd87e50aba13a
Martin <marttkip@gmail.com>
2020-12-01 15:04:54 +0300

    new changes

--- application/modules/messaging/controllers/messaging.php
+++ application/modules/messaging/controllers/messaging.php
@@ -1302,7 +1302,8 @@ class Messaging extends MX_Controller
 				$time_start = $value->time_start;
 				$visit_date = date('jS M Y',strtotime($date_tomorrow));
 				// $time_start = date('H:i A',strtotime($time_start));
-				$message = 'Hello '.$patient_surname.', please remember that you have a dental appointment scheduled for '.$date_to_send.' '.$visit_date.' at '.$time_start.'. For more information contact 0717123440.';
+				$message = 'Hello '.$patient_surname.', please remember that you have a dental appointment scheduled for '.$date_to_send.' '.$visit_date.' at '.$time_start.' Eastleigh Mall room 101. For more information contact 0717123440.';
+
 				// $patient_phone = 734808007;
 				$message_data = array(
 						"phone_number" => $patient_phone,
